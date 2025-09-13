a3ffde034aaed0cf3f79f64905f4de8417f14938
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-09-13 11:39:55 +0200

    quering the pgDB works

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
-import { config } from "dotenv";
-config({ silent: true });
-
+import { MongoClient } from "mongodb";
+import { config as configDotenv } from "dotenv";
+configDotenv({ silent: true });
 const url = process.env.MONGODB_URL;
-const client = new MongoClient(url);
+const clientMongo = new MongoClient(url);
+
+import { Client } from "pg";
+const config = {
+  host: "localhost",
+  user: "hyfuser",
+  password: "hyfpassword",
+  database: "prepExDBw3",
+  port: 5432,
+};
+const clientPG = new Client(config);
+let queryRes;
+
+clientPG.connect();
+queryRes = await clientPG.query("SELECT * FROM recipes;");
+console.log("\nrecipes:", JSON.stringify(queryRes.rows, null, 2));
+clientPG.end();
 
 async function createCollection() {
   try {
-    await client.connect();
-    const db = client.db("prepExDBw4");
+    await clientMongo.connect();
+    const db = clientMongo.db("prepExDBw4");
     await db.createCollection("myCollection");
     console.log("Collection created!");
   } catch (err) {
     console.error("Error creating collection:", err);
   } finally {
-    await client.close();
+    await clientMongo.close();
   }
 }
 
